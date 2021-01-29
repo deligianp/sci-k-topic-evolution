d8a187632875a0f6bb5facd8b901cfb80fa823e2
deligianp <deligianp@athenarc.gr>
2021-01-29 09:56:29 +0200

    Fix bug for model_topics endpoint for case of topic 0

--- topic_evolution_visualization/api/views.py
+++ topic_evolution_visualization/api/views.py
@@ -10,21 +10,20 @@ def model_topics(request):
     target_model_name = request.GET.get("name", None)
     offset = request.GET.get("offset", 0)
     limit = request.GET.get("limit", 50)
-    topic = request.GET.get("topic", None)
+    topic = int(request.GET.get("topic", -1))
     data = dict()
     main_model = queries.get_model(name=target_model_name)
     if main_model:
         topics = queries.get_topics_terms_representation(
             main_model,
-            *(topic,) if topic else tuple(range(offset, offset + limit))
+            *(topic,) if topic >= 0 else tuple(range(offset, offset + limit))
         )
-        if topic:
-            data = {
-                "name": main_model.name,
-                "description": main_model.description,
-                "training_context": main_model.training_context,
-                "topics": topics
-            }
+        data = {
+            "name": main_model.name,
+            "description": main_model.description,
+            "training_context": main_model.training_context,
+            "topics": topics
+        }
     return Response(data)
 
 
